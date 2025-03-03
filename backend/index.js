c627eb17cf280a1b652de2ed2dbb2a1e7e929082
Shewon Gunarathne <shewongunarathne@gmail.com>
2025-03-03 14:20:41 +0530

    backend second commit

--- backend/index.js
+++ backend/index.js
@@ -5,14 +5,14 @@ const mongoose = require("mongoose")
 
 mongoose.connect(config.connectionString)
 
-const User = require("./models/user.model")
+const User = require("./models/user.model.js")
 
 const express = require("express")
 const cors = require("cors")
 const app = express()
 
 const jwt = require("jsonwebtoken")
-const {authenticateToken} = require("./utilities")
+const {authenticateToken} = require("./utilities.js")
 
 app.use(express.json())
 
@@ -61,6 +61,38 @@ app.post("/create-account", async (req, res) => {
     })
 
     return res.json({error: false, user, accessToken, message: "Registration Successful"})
+})
+
+// Login
+app.post("/login", async (req, res) => {
+    const {email, password} = req.body;
+
+
+    if(!email){
+        return res.status(400).json({ message: "Email is required"})
+    }
+
+    if(!password){
+        return res.status(400).json({ message: "Password is required"})
+    }
+
+    const userInfo = await User.findOne({email : email})
+
+    if(!userInfo){
+        return res.status(400).json({message: "User not found"})
+    }
+
+    if (userInfo.email == email && userInfo.password == password){
+        const user = {user: userInfo}
+        const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "36000m",
+        })
+
+        return res.json({error: false, message: "Login Successful", email, accessToken,})
+    }
+    else{
+        return res.json({error: true, message: "Inavlid Credentials",})
+    }
 })
 
 app.listen(8000)
